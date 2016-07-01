11f0090a118ff1b7b757169788b27e7e2a08f8cd
shekhar madnawat <shekhar.madnawat@mobiloittegroup.com>
2016-07-01 17:11:15 +0530

    configure sidekiq

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
-worker: bundle exec sidekiq -e production
+worker: bundle exec sidekiq -e production -C config/sidekiq.yml
 
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 Sidekiq.configure_server do |config|
+	config.redis = { :size => 2 }
   config.redis = { url: ENV["REDISTOGO_URL"] }
 end unless ENV["REDISTOGO_URL"].blank?
 
 Sidekiq.configure_client do |config|
+	config.redis = { :size => 1 }
   config.redis = { url: ENV["REDISTOGO_URL"] }
 end unless ENV["REDISTOGO_URL"].blank?
