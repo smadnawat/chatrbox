6257c7087e6864ca66b4dcd776fe6f3d1e12f7e2
shekhar madnawat <shekhar.madnawat@mobiloittegroup.com>
2016-07-01 18:17:26 +0530

    redis.rb added

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,7 @@
 Sidekiq.configure_server do |config|
-	config.redis = { :size => 2 }
   config.redis = { url: ENV["REDISTOGO_URL"] }
 end unless ENV["REDISTOGO_URL"].blank?
 
 Sidekiq.configure_client do |config|
-	config.redis = { :size => 1 }
   config.redis = { url: ENV["REDISTOGO_URL"] }
 end unless ENV["REDISTOGO_URL"].blank?
